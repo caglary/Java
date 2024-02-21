95dcec0bc04815b087a3327f2c29b490a1d3472a
çağlar yurdakul <37529684+caglary@users.noreply.github.com>
2024-02-21 16:19:01 +0300

    first commit

---
+++ Welcome/src/FirstProject/FirstProgram.java
@@ -0,0 +1,15 @@
+package FirstProject;
+
+public class FirstProgram { //By convention, class names start with an uppercase letter
+
+	public static void main(String[] args) {//The program is executed from the main method.
+		System.out.println("Welcome to Java"); //Every statement in Java ends with a semicolon (;), known as the statement terminator.
+	
+		System.out.println("(10.5+2*3)/(45-3.5)");
+		System.out.println((10.5+2*3)/(45-3.5));
+		
+		//last page 38 
+		
+	}
+
+}
