b0c60fec6dd4721a253c677fdf777f099799e475
caglary <caglar.yurdakul60@gmail.com>
2024-03-24 03:03:47 +0300

    from ubuntu

--- Welcome/src/TesterCode.java
+++ Welcome/src/TesterCode.java
@@ -7,27 +7,37 @@ public class TesterCode {
 
 	public static void main(String[] args) {
 		
-		Product product=new Product();
+//		Product product=new Product();
+//		
+//		product.setDescription("Asus");
+//		product.setName("a-11");
+//		product.setPrice(-11);
+//		
+//		
+//		ProductManager productManager=new ProductManager();
+//		productManager.add(product);
+//		System.out.println(product.getId());
+//
+//		Product product2=new Product();
+//		
+//		product2.setDescription("Hp");
+//		product2.setName("11BB");
+//		product2.setPrice(122);
+//
+//		productManager.add(product2);
+//		System.out.println(product2.getId());
+//		
+//		Customer customer=new Customer();
+//		customer.firstName="ali";
+//		Employee employee=new Employee();
+//		employee.firstName="Çalışan ahmet";
 		
-		product.setDescription("Asus");
-		product.setName("a-11");
-		product.setPrice(-11);
+		EmployeeManager employeeManager=new EmployeeManager();
+		CustomerManager customerManager=new CustomerManager();
+		employeeManager.bestEmployee();
+		customerManager.add();
 		
 		
-		ProductManager productManager=new ProductManager();
-		productManager.add(product);
-		System.out.println(product.getId());
-
-		Product product2=new Product();
-		
-		product2.setDescription("Hp");
-		product2.setName("11BB");
-		product2.setPrice(122);
-
-		productManager.add(product2);
-		System.out.println(product2.getId());
-		
-
 	}
 	
 
